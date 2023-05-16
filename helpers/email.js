0697fb99900de2ff7168423c4f87b9b0488159fe
eneko9555 <eneko_55@hotmail.com>
2023-05-16 14:17:00 +0200

    smpt

--- helpers/email.js
+++ helpers/email.js
@@ -14,11 +14,11 @@ export const emailRegister = async data => {
     });
 
     const info = await transport.sendMail({
-        from: '"UpTask - Administrador de Proyectos" <cuentas@uptask.com',
+        from: '"Pmanager - Administrador de Proyectos" <cuentas@Pmanager.com',
         to:email,
-        subject: "UpTask -  Confirma tu Cuenta",
-        text: "Comprueba tu cuenta en UpTask",
-        html: `<p>Bienvenido ${name}, comprueba tu cuenta en UpTask </p>
+        subject: "Pmanager -  Confirma tu Cuenta",
+        text: "Comprueba tu cuenta en Pmanager",
+        html: `<p>Bienvenido ${name}, comprueba tu cuenta en Pmanager </p>
         <p>Tu cuenta ya esta casi lista, debes comprobarla en el siguiente enlace : 
         <a href="${process.env.FRONTEND_URL}/confirmar-cuenta/${token}">Comprobar Cuenta</a>
         <p>Si tu no creaste esta cuenta, puedes ignorar este mensaje</p>
@@ -39,11 +39,11 @@ export const emailResetPassword = async data => {
     });
 
     const info = await transport.sendMail({
-        from: '"UpTask - Administrador de Proyectos" <cuentas@uptask.com',
+        from: '"Pmanager - Administrador de Proyectos" <cuentas@Pmanager.com',
         to:email,
-        subject: "UpTask -  Restablecer contraseña",
-        text: "Restablece tu contraseña en UpTask",
-        html: `<p>Hola ${name}, has solicitado recuperar tu contraseña  en UpTask </p>
+        subject: "Pmanager -  Restablecer contraseña",
+        text: "Restablece tu contraseña en Pmanager",
+        html: `<p>Hola ${name}, has solicitado recuperar tu contraseña  en Pmanager </p>
         <p>Sigue el siguiente enlace para generar una nueva contraseña: 
         <a href="${process.env.FRONTEND_URL}/recuperar-contraseña/${token}">Restablecer contraseña</a>
         <p>Si tu no solicitaste este email, puedes ignorar este mensaje</p>
